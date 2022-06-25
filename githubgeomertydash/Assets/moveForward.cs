51b088615c18478a011acf22028136e47f7b0f14
rishi423 <108159647+rishi423@users.noreply.github.com>
2022-06-25 15:45:12 -0700

    Jump code fixed
    I fixed the jump code using a YouTube tutorial with rigid body physics as opposed to your way of moving forward. Also, based on the YouTube tutorial, I found a collision function and through a boolean variable I fixed the issue of double jump as well.

--- githubgeomertydash/Assets/moveForward.cs
+++ githubgeomertydash/Assets/moveForward.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class moveForward : MonoBehaviour
 {
     public float speed;
+    public Rigidbody rb;
+    public bool CubeOnGround = true; 
     // Start is called before the first frame update
     private void Start()
     {
-
+      rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,11 +20,17 @@ public class moveForward : MonoBehaviour
         //moves forward at speed or var speed
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        if(Input.GetButtonDown("Jump")){
-        //jump code
+        if(Input.GetButtonDown("Jump") && CubeOnGround == true){
+            //jump code
         
-        transform.Translate(Vector3.up * Time.deltaTime * speed * 20);
+            rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            CubeOnGround = false;
 
         }
     }
+   private void OnCollisionEnter(Collision collision){
+      if(collision.gameObject.name == "basecube"){
+          CubeOnGround = true;
+      }
+   }
 }
