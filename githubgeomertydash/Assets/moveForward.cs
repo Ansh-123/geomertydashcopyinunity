93fd62f94bf81ee8a9b2f079a4546e21b87f01d2
rishi423 <108159647+rishi423@users.noreply.github.com>
2022-06-26 16:52:26 -0700

    the kill code doesn't work
    we need to fix the kill and respawn code.

--- githubgeomertydash/Assets/moveForward.cs
+++ githubgeomertydash/Assets/moveForward.cs
@@ -2,35 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveForward : MonoBehaviour
+public class moveForward: MonoBehaviour
 {
-    public float speed;
-    public Rigidbody rb;
-    public bool CubeOnGround = true; 
-    // Start is called before the first frame update
-    private void Start()
-    {
-      rb = GetComponent<Rigidbody>();
-    }
+   public float speed;
+   public Rigidbody rb;
+   public bool CubeOnGround = true;
+   //Start is called before the first frame update
+   private void Start()
+   {
+       rb = GetComponent<Rigidbody>();
+   }
+   
+   //Update is called once per frame
+   void Update()
+   {
+       //moves forward at speed or var speed
+       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       
+       if(Input.GetButtonDown("Jump") && CubeOnGround == true){
+           //jump code
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        //moves forward at speed or var speed
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+           rb.AddForce(new Vector3(0, 30, 0), ForceMode.Impulse);
+           CubeOnGround = false;       
 
-        if(Input.GetButtonDown("Jump") && CubeOnGround == true){
-            //jump code
-        
-            rb.AddForce(new Vector3(0, 20, 0), ForceMode.Impulse);
-            CubeOnGround = false;
+       }
 
-        }
-    }
+   }
    private void OnCollisionEnter(Collision collision){
-      if(collision.gameObject.name == "basecube"){
-          CubeOnGround = true;
-      }
+       if(collision.gameObject.name != "pointyspike"){
+           CubeOnGround = true;
+       }
+   
    }
 }
