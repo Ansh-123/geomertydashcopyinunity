e76c212cd5efe18305e90ea371586ffc90035361
rishi423 <108159647+rishi423@users.noreply.github.com>
2022-06-27 09:26:05 -0700

    the stupid solution to the death code
    alright, so this does in fact work now (u can playtest if u want, it's good). every spike kills the cube. however, it's a shamefully bad solution, if you can't tell from the code, just look at the hierarchy bar and tell me if it's a big issue. we also want to add the death animation in RespawnAndKillMe.cs.

--- githubgeomertydash/Assets/CameraReset.cs
+++ githubgeomertydash/Assets/CameraReset.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnAndKillMe : MonoBehaviour
+public class CameraReset : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private Transform camera;
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform cameraRespawnPoint;
     
     void OnCollisionEnter(Collision collision)
     {
       if(collision.gameObject.name == "RespawnTrigger" || collision.gameObject.name == "pointyspike Variant"){
-          //death animation needs to go here
-          player.transform.position = respawnPoint.transform.position;
+          camera.transform.position = cameraRespawnPoint.transform.position;
       }
     }
 }
